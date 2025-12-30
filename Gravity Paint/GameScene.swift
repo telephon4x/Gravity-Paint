4d5d155769911221d43abce79ab5f79b0fb3842e
telephon4x <jadenwright5x@gmail.com>
2025-12-29 18:23:19 -0600

    Tap spawns physics particles

--- Gravity Paint/GameScene.swift
+++ Gravity Paint/GameScene.swift
@@ -10,75 +10,58 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
+        backgroundColor = .black
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
+
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsBody?.friction = 0.2
+
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        guard let touch = touches.first else { return } 
+        let p = touch.location(in: self)
+        spawnParticles(at: p)
+    }
+
+    private func spawnParticles(at position: CGPoint) {
+        let radius: CGFloat = 10
+        let node = SKShapeNode(circleOfRadius: radius)
+
+        node.position = position
+        node.fillColor = .blue
+        node.strokeColor = .clear
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        node.physicsBody = SKPhysicsBody(circleOfRadius: radius)
+        node.physicsBody?.restitution = 0.8
+        node.physicsBody?.friction = 0.05
+        node.physicsBody?.linearDamping = 0.05
+        node.physicsBody?.angularDamping = 0.05
+        
+        addChild(node)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     
